e7f6745b54fc16d9160f38482232b954e4a1d652
Strus <strus155@gmail.com>
2023-04-10 20:44:21 +0200

    Switch telescope to 0.1.x branch

--- lua/strus/packer.lua
+++ lua/strus/packer.lua
@@ -3,7 +3,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        'nvim-telescope/telescope.nvim', branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
     use { 'nvim-telescope/telescope-fzf-native.nvim',
