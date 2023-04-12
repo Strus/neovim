a85e98b0a6843f5d182685105814f822d6c7a743
Strus <strus155@gmail.com>
2023-04-12 16:19:08 +0200

    Implement workaround for sematic highlighting in onedark

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -12,3 +12,23 @@ require('onedark').setup({
         git = { change = '#e0af68', add = '#109868', delete = '#9A353D', conflict = '#bb7a61' },
     }
 })
+
+local links = {
+  ['@lsp.type.namespace'] = '@namespace',
+  ['@lsp.type.type'] = '@type',
+  ['@lsp.type.class'] = '@type',
+  ['@lsp.type.enum'] = '@type',
+  ['@lsp.type.interface'] = '@type',
+  ['@lsp.type.struct'] = '@structure',
+  ['@lsp.type.parameter'] = '@parameter',
+  ['@lsp.type.variable'] = '@variable',
+  ['@lsp.type.property'] = '@property',
+  ['@lsp.type.enumMember'] = '@constant',
+  ['@lsp.type.function'] = '@function',
+  ['@lsp.type.method'] = '@method',
+  ['@lsp.type.macro'] = '@macro',
+  ['@lsp.type.decorator'] = '@function',
+}
+for newgroup, oldgroup in pairs(links) do
+  vim.api.nvim_set_hl(0, newgroup, { link = oldgroup, default = true })
+end
