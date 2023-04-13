b4fa858ec3fed72d975a18966a1670182a238445
Adrian Kepka <akepka@box.com>
2023-04-13 17:59:54 +0200

    Group plugins in packer.lua

--- lua/strus/packer.lua
+++ lua/strus/packer.lua
@@ -2,6 +2,8 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
+
+    -- navigation
     use {
         'nvim-telescope/telescope.nvim', branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
@@ -10,13 +12,43 @@ return require('packer').startup(function(use)
         run =
         'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
     use 'barrett-ruth/telescope-http.nvim'
+    use 'theprimeagen/harpoon'
+    use {
+        "SmiteshP/nvim-navbuddy",
+        requires = {
+            "neovim/nvim-lspconfig",
+            "SmiteshP/nvim-navic",
+            "MunifTanjim/nui.nvim"
+        }
+    }
+    use 'nvim-tree/nvim-tree.lua'
+    use {
+        'ggandor/leap.nvim',
+        config = function()
+            require('leap').add_default_mappings()
+        end,
+    }
+    use {
+        "folke/trouble.nvim",
+        config = function()
+            require("trouble").setup {
+                mode = "document_diagnostics"
+            }
+        end
+    }
+
+    -- themes
     use 'monsonjeremy/onedark.nvim'
+    use 'EdenEast/nightfox.nvim'
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
+    use { "catppuccin/nvim", as = "catppuccin" }
+    use 'sainnhe/everforest'
+
+    -- code-specific (lsp etc.)
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
     }
-    use 'theprimeagen/harpoon'
-    use 'mbbill/undotree'
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v1.x',
@@ -39,47 +71,38 @@ return require('packer').startup(function(use)
             { 'rafamadriz/friendly-snippets' }, -- Optional
         }
     }
-    use {
-        "SmiteshP/nvim-navbuddy",
-        requires = {
-            "neovim/nvim-lspconfig",
-            "SmiteshP/nvim-navic",
-            "MunifTanjim/nui.nvim"
-        }
-    }
-    use 'j-hui/fidget.nvim'
-    use 'smithbm2316/centerpad.nvim'
-    use { 'shortcuts/no-neck-pain.nvim', tag = '*' }
-    use 'kevinhwang91/nvim-bqf'
-    use 'nvim-tree/nvim-web-devicons'
-    use 'nvim-tree/nvim-tree.lua'
-    use {
-        "ThePrimeagen/refactoring.nvim",
-        requires = {
-            { "nvim-lua/plenary.nvim" },
-            { "nvim-treesitter/nvim-treesitter" }
-        }
-    }
     use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
+    use 'j-hui/fidget.nvim'
     use { 'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim' }
     use { 'simrat39/rust-tools.nvim' }
-    use { "ahmedkhalf/project.nvim" }
-    use 'terrortylor/nvim-comment'
-    use {
-        "windwp/nvim-autopairs",
-        config = function() require("nvim-autopairs").setup {} end
-    }
+    use 'jose-elias-alvarez/null-ls.nvim'
+    use 'rust-lang/rust.vim'
+    use 'mfussenegger/nvim-dap-python'
+    use 'Vimjas/vim-python-pep8-indent'
+
+    -- ui improvements
+    use { 'fgheng/winbar.nvim' }
+    use { 'petertriho/nvim-scrollbar' }
+    use { 'shortcuts/no-neck-pain.nvim', tag = '*' }
+    use 'kevinhwang91/nvim-bqf'
+    use 'nvim-lualine/lualine.nvim'
     use({
         "luukvbaal/statuscol.nvim",
         config = function() require("statuscol").setup() end,
         commit = "5269fb1220f0909c82be8a0c9eab657a55a5f1fa",
     })
-    use "tpope/vim-fugitive"
+    use {
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require('gitsigns').setup()
+            require("scrollbar.handlers.gitsigns").setup()
+        end
+    }
+    use { 'weilbith/nvim-code-action-menu', cmd = "CodeActionMenu" }
+
+    -- code testing
     use 'vim-test/vim-test'
-    use 'tpope/vim-dispatch'
     use 'okcompute/vim-nose'
-    use 'airblade/vim-rooter'
-    use 'jandamm/vim-projplugin'
     use {
         "nvim-neotest/neotest",
         requires = {
@@ -89,17 +112,18 @@ return require('packer').startup(function(use)
             "nvim-neotest/neotest-python",
         }
     }
-    use 'Shougo/denite.nvim'
+
+    -- editing
+    use 'terrortylor/nvim-comment'
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
     use {
         "danymat/neogen",
         requires = "nvim-treesitter/nvim-treesitter",
         tag = "*"
     }
-    use 'jose-elias-alvarez/null-ls.nvim'
-    use 'rust-lang/rust.vim'
-    use 'nvim-lualine/lualine.nvim'
-    use { 'fgheng/winbar.nvim' }
-    use { "akinsho/toggleterm.nvim", tag = '*' }
     use({
         "kylechui/nvim-surround",
         tag = "*",
@@ -107,46 +131,37 @@ return require('packer').startup(function(use)
             require("nvim-surround").setup()
         end
     })
+    use {
+        "ThePrimeagen/refactoring.nvim",
+        requires = {
+            { "nvim-lua/plenary.nvim" },
+            { "nvim-treesitter/nvim-treesitter" }
+        }
+    }
+
+    -- project management
+    use { "ahmedkhalf/project.nvim" }
+    use 'jandamm/vim-projplugin'
+    use 'airblade/vim-rooter'
+
+    -- misc
+    use 'wakatime/vim-wakatime'
+    use 'mbbill/undotree'
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function() vim.fn["mkdp#util#install"]() end,
+    })
     use({
         'asiryk/auto-hlsearch.nvim',
         config = function()
             require("auto-hlsearch").setup()
         end
     })
-    use {
-        "folke/trouble.nvim",
-        config = function()
-            require("trouble").setup {
-                mode = "document_diagnostics"
-            }
-        end
-    }
-    use 'wakatime/vim-wakatime'
-    use 'mfussenegger/nvim-dap-python'
-    use 'Vimjas/vim-python-pep8-indent'
-    use {
-        'lewis6991/gitsigns.nvim',
-        config = function()
-            require('gitsigns').setup()
-            require("scrollbar.handlers.gitsigns").setup()
-        end
-    }
-    use { 'weilbith/nvim-code-action-menu', cmd = "CodeActionMenu" }
-    use({
-        "iamcco/markdown-preview.nvim",
-        run = function() vim.fn["mkdp#util#install"]() end,
-    })
-    use { 'petertriho/nvim-scrollbar' }
-    -- use {
-    --     "chrisgrieser/nvim-early-retirement",
-    --     config = function()
-    --         require("early-retirement").setup()
-    --     end,
-    -- }
-    use {
-        'ggandor/leap.nvim',
-        config = function()
-            require('leap').add_default_mappings()
-        end,
-    }
+    use "tpope/vim-fugitive"
+    use { "akinsho/toggleterm.nvim", tag = '*' }
+
+    -- plugin utilities
+    use 'tpope/vim-dispatch'
+    use 'nvim-tree/nvim-web-devicons'
+    use 'Shougo/denite.nvim'
 end)
