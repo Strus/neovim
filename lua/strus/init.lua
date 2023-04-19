b858ec720488187b6637aa5c7a21220c474a417f
Adrian Kepka <kepka.adrian@gmail.com>
2023-04-19 06:44:04 +0200

    Add auto save of all buffers every 5 minutes

--- lua/strus/init.lua
+++ lua/strus/init.lua
@@ -2,3 +2,7 @@ require('strus.remap')
 require('strus.set')
 require('strus.packer')
 
+local timer = vim.loop.new_timer()
+timer:start(5 * 60 * 1000, 5 * 60 * 1000, vim.schedule_wrap(function()
+    vim.cmd("wa")
+end))
