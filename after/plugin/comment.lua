ebbd3900c9f24917f1912bf84353ec1017ff2f17
Strus <strus155@gmail.com>
2023-04-21 22:18:01 +0200

    Fix comment lua autocmd

--- after/plugin/comment.lua
+++ after/plugin/comment.lua
@@ -6,7 +6,7 @@ vim.keymap.set('v', '<C-/>', ':CommentToggle<CR>', { silent = true })
 vim.keymap.set('n', '<C-/>', ':CommentToggle<CR>j', { silent = true })
 
 
-vim.api.nvim_create_autocmd("BufEnter,BufFilePost", {
+vim.api.nvim_create_autocmd({ "BufEnter", "BufFilePost" }, {
     pattern = "*.c,*.cpp,*.h,*.hpp",
     callback = function()
         vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
