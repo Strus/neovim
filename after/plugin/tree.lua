c4d1e9aed117ea8d707c0c8c3aa9818986f4cb2a
Strus <strus155@gmail.com>
2023-10-10 12:17:18 +0200

    Update mappings in nvim-tree to use on_attach

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -1,6 +1,30 @@
 local tree = require('nvim-tree')
 local tree_api = require('nvim-tree.api')
 
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', '<LeftRelease>', api.tree.edit, opts('Edit'))
+  vim.keymap.set('n', '<Esc>', api.tree.close, opts('Close'))
+  vim.keymap.set('n', '<M-1>', api.tree.close, opts('Close'))
+  vim.keymap.set('n', '<M-!>', api.tree.close, opts('Close'))
+end
+
+-- pass to setup along with your other options
+require("nvim-tree").setup {
+  ---
+  on_attach = my_on_attach,
+  ---
+}
+
 tree.setup({
   view = {
     width = 50,
@@ -13,14 +37,6 @@ tree.setup({
         height = 66,
       }
     },
-    mappings = {
-      list = {
-        { key = '<LeftRelease>', action = 'edit' },
-        { key = '<Esc>',         action = 'close' },
-        { key = '<M-1>',         action = 'close' },
-        { key = '<M-!>',         action = 'close' },
-      }
-    }
   },
   git = {
     enable = false,
