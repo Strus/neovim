7bfe389d7f22cbef71278413b83055ab63221da0
Strus <strus155@gmail.com>
2023-10-21 07:52:33 +0200

    Bootstrap packer

--- lua/strus/packer.lua
+++ lua/strus/packer.lua
@@ -1,4 +1,13 @@
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd [[packadd packer.nvim]]
+  end
+end
+
+ensure_packer()
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
@@ -156,27 +165,6 @@ return require('packer').startup(function(use)
     config = function()
       require("auto-hlsearch").setup()
     end
-  })
-  use "tpope/vim-fugitive"
-  use { "akinsho/toggleterm.nvim", tag = '*' }
-
-  -- project management
-  use { "ahmedkhalf/project.nvim" }
-  use 'jandamm/vim-projplugin'
-  use 'airblade/vim-rooter'
-
-  -- misc
-  use 'wakatime/vim-wakatime'
-  use 'mbbill/undotree'
-  use({
-      "iamcco/markdown-preview.nvim",
-      run = function() vim.fn["mkdp#util#install"]() end,
-  })
-  use({
-      'asiryk/auto-hlsearch.nvim',
-      config = function()
-          require("auto-hlsearch").setup()
-      end
   })
   use "tpope/vim-fugitive"
   use { "akinsho/toggleterm.nvim", tag = '*' }
