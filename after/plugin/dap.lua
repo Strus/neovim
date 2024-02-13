7385529c4d2e6bdeaf8545e97df9883074e3575a
Adrian Kepka <akepka@box.com>
2024-02-13 14:41:47 +0100

    Add ability to run current Python file with DAP

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -60,3 +60,21 @@ vim.fn.sign_define('DapStopped', { text = '', texthl = 'DapStopped', linehl =
 
 vim.keymap.set('n', '<leader>db', ':DapToggleBreakpoint<CR>', { silent = true })
 vim.keymap.set('n', '<leader>dl', ':lua require("dap").list_breakpoints()<CR>:copen<CR>', { silent = true })
+
+
+dap.adapters.python = {
+    type = 'executable',
+    command = 'python',
+    args = { '-m', 'debugpy.adapter' },
+}
+
+dap.configurations.python = {
+    {
+        -- The first three options are required by nvim-dap
+        type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
+        request = 'launch',
+        name = 'Run',
+        -- -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+        program = vim.fn.expand("%:p"),
+    },
+}
