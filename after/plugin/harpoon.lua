76b66124e44a928fba658e7e701dd20ea77bf86a
Strus <strus155@gmail.com>
2024-02-13 14:41:24 +0100

    Fix Harpoon and add new bindings

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -3,9 +3,17 @@ local ui = require("harpoon.ui")
 
 require("harpoon").setup({
   menu = {
-    width = vim.api.nvim_win_get_width(0) * 0.5,
+    width = 80.0,
   }
 })
 
 vim.keymap.set("n", "<leader>ha", mark.add_file)
 vim.keymap.set("n", "<leader>hh", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<leader>hi", ui.nav_prev)
+vim.keymap.set("n", "<leader>ho", ui.nav_next)
+vim.keymap.set("n", "<leader>hq", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>hw", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>he", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>hr", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>ht", function() ui.nav_file(5) end)
