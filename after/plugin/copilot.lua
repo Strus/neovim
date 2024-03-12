605e27d0e91c4bb4ba0f64c32e5dfccc4291c02d
Strus <strus155@gmail.com>
2024-03-12 21:27:05 +0100

    Change copilot to standard plugin and add chat

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,8 +1,20 @@
-require("copilot").setup({
-  suggestion = {
-    auto_trigger = false,
-    -- keymap = {
-    --     accept = "<C-f>",
-    -- },
-  },
+-- require("copilot").setup({
+--   suggestion = {
+--     auto_trigger = false,
+--     keymap = {
+--       accept = "<C-l>",
+--     },
+--   },
+-- })
+--
+vim.keymap.set('i', '<C-l>', 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false
 })
+vim.g.copilot_no_tab_map = true
+vim.cmd([[let g:copilot_filetypes = { '*': v:false }]])
+
+require("CopilotChat").setup()
+
+vim.keymap.set({ 'n', 'v' }, '<leader>ac', ':CopilotChat<CR>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>ae', ':CopilotChatExplain<CR>', { silent = true })
