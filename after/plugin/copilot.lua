48070c8e244f080b8d87a0bbd6801b9a5c192387
Adrian Kepka <akepka@box.com>
2024-12-26 13:52:46 +0100

    Update copilot configuration

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,18 +1,9 @@
--- require("copilot").setup({
---   suggestion = {
---     auto_trigger = false,
---     keymap = {
---       accept = "<C-l>",
---     },
---   },
--- })
---
 vim.keymap.set('i', '<C-l>', 'copilot#Accept("\\<CR>")', {
   expr = true,
   replace_keycodes = false
 })
 vim.g.copilot_no_tab_map = true
--- vim.cmd([[let g:copilot_filetypes = { '*': v:false }]])
+vim.cmd([[let g:copilot_filetypes = { '*': v:false }]])
 
 vim.keymap.set({ 'n', 'v' }, '<leader>ac', ':CopilotChat<CR>', { silent = true })
 vim.keymap.set({ 'n', 'v' }, '<leader>ae', ':CopilotChatExplain<CR>', { silent = true })
