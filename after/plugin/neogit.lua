f206071fd4a8af655637fd4d33863d6ebd7b0ec6
Strus <strus155@gmail.com>
2026-01-08 10:12:08 +0100

    Fix neogit config

--- after/plugin/neogit.lua
+++ after/plugin/neogit.lua
@@ -1,8 +1,16 @@
 require("neogit").setup({
   disable_context_highlighting = true,
   git_services = {
-    ["git.dev.box.net"] = "https://git.dev.box.net/${owner}/${repository}/compare/${branch_name}?expand=1",
-    ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
+    ["git.dev.box.net"] = {
+      pull_request = "https://git.dev.box.net/${owner}/${repository}/compare/${branch_name}?expand=1",
+      commit = "https://git.dev.box.net/${owner}/${repository}/commit/${oid}",
+      tree = "https://${host}/${owner}/${repository}/tree/${branch_name}",
+    },
+    ["github.com"] = {
+      pull_request = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
+      commit = "https://github.com/${owner}/${repository}/commit/${oid}",
+      tree = "https://${host}/${owner}/${repository}/tree/${branch_name}",
+    },
   },
   kind = "split",
   sections = {
