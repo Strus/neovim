5421a5247145d64c8c565bef1ad4086ad30da56e
Strus <strus155@gmail.com>
2026-04-28 20:34:58 +0200

    Update nvim completion keybinding

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -93,10 +93,9 @@ cmp.setup({
     { name = 'nvim_lsp_signature_help' },
   },
   mapping = cmp.mapping.preset.insert {
-
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-l>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-n>'] = cmp.mapping.complete(),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Insert,
       select = true,
